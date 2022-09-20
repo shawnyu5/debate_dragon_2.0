c4f1c1b167400872928f5f434efe24bf3cc6dfdb
shawn yu <shawn.yu5@hotmail.com>
2022-09-19 20:41:17 -0400

    feat(utils): ParseUserOptions
    Parses all user options to a command and return a map

--- utils/utils.go
+++ utils/utils.go
@@ -39,3 +39,13 @@ func RemoveCommands(dg *discordgo.Session, registeredCommands []*discordgo.Appli
 		}
 	}
 }
+
+// ParseUserOptions parses the user option passed to a command, and returns a map of data options
+func ParseUserOptions(sess *discordgo.Session, i *discordgo.InteractionCreate) map[string]*discordgo.ApplicationCommandInteractionDataOption {
+	options := i.ApplicationCommandData().Options
+	optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+	for _, opt := range options {
+		optionMap[opt.Name] = opt
+	}
+	return optionMap
+}
