2deb7b8f8b97f2b169290d844564170d6857c2c4
shawn yu <shawn.yu5@hotmail.com>
2022-09-27 23:19:16 -0400

    refactor(utils): `LoadConfig()`
    Have a function that loads the config from config.json

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
@@ -20,18 +18,7 @@ var dg *discordgo.Session
 
 // init reads config.json and sets global config variable
 func init() {
-	// read json file
-	f, err := os.Open("config.json")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	b, err := ioutil.ReadAll(f)
-	if err != nil {
-		panic(err)
-	}
-	json.Unmarshal(b, &c)
+	c = utils.LoadConfig()
 }
 
 func init() {
