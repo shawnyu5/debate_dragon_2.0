80964ca57b5f99986be68e39f0054333491bd357
shawn yu <shawn.yu5@hotmail.com>
2022-09-30 21:12:33 -0400

    refactor: main.go => commands.go

--- commands/commands.go
+++ commands/commands.go
@@ -12,10 +12,3 @@ type CommandStruct struct {
 	// component handler
 	ComponentHandler func(sess *discordgo.Session, i *discordgo.InteractionCreate)
 }
-
-// type Command interface {
-// // command handler function
-// Handler(s *discordgo.Session, i *discordgo.InteractionCreate)
-// // return a command object
-// Obj() *discordgo.ApplicationCommandOption
-// }
