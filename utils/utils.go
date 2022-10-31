81e41dca69562a4d79c2f029b4133ae913a7b4e8
shawn yu <shawn.yu5@hotmail.com>
2022-10-30 22:22:17 -0400

    fix: delete slash commands

--- utils/utils.go
+++ utils/utils.go
@@ -37,11 +37,25 @@ type Config struct {
 // RegisterCommands register an array of commands to a discord session.
 // Receives an instance of discord session to store commands in. An array of discord application commands to keep track of the stored commands. And an array of commands to register
 // Will panic if registration of a command fails.
-func RegisterCommands(dg *discordgo.Session, commands []*discordgo.ApplicationCommand, registeredCommands []*discordgo.ApplicationCommand) {
+func RegisterCommands(sess *discordgo.Session, commands []*discordgo.ApplicationCommand, registeredCommands []*discordgo.ApplicationCommand) {
+	// c := LoadConfig()
 	log.Println("Adding commands...")
-	for _, gld := range dg.State.Guilds {
+	for _, gld := range sess.State.Guilds {
+		// roles := gld.Roles
+		fmt.Println(gld.Name)
+		// if c.Development {
+		// for _, role := range roles {
+		// // if bot role is not at the top 2, dont register commmands here
+		// fmt.Printf("RegisterCommands role.Name: %v\n", role.Name)         // __AUTO_GENERATED_PRINT_VAR__
+		// fmt.Printf("RegisterCommands role.Position: %v\n", role.Position) // __AUTO_GENERATED_PRINT_VAR__
+		// if role.Name == "debate dragon alpha" && role.Position > 3 {
+		// log.Printf("Bot role is not at the top 2, not registering commands in guild %v", gld.Name)
+		// continue
+		// }
+		// }
+		// }
 		for i, v := range commands {
-			cmd, err := dg.ApplicationCommandCreate(dg.State.User.ID, gld.ID, v)
+			cmd, err := sess.ApplicationCommandCreate(sess.State.User.ID, gld.ID, v)
 			if err != nil {
 				log.Panicf("Cannot create '%v' command: %v", v.Name, err)
 			}
@@ -58,7 +72,7 @@ func RemoveCommands(sess *discordgo.Session, registeredCommands []*discordgo.App
 	for _, gld := range sess.State.Guilds {
 		for _, cmd := range registeredCommands {
 			// TODO: fix this
-			err := sess.ApplicationCommandDelete(cmd.ApplicationID, cmd.ID, gld.ID)
+			err := sess.ApplicationCommandDelete(sess.State.User.ID, gld.ID, cmd.ID)
 			// err := sess.ApplicationCommandDelete(cmd.ApplicationID, cmd.ID, "")
 			// err := sess.ApplicationCommandDelete(sess.State.User.ID, gld.ID, cmd.ID)
 			if err != nil {
