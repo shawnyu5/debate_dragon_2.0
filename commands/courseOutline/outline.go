9a02fbd32ec316b4c67ccd6462369d81f982bcd9
shawn yu <shawn.yu5@hotmail.com>
2022-12-03 22:07:31 -0500

    feat: `/outline`
    gets a course outline for a course

---
+++ commands/courseOutline/outline.go
@@ -0,0 +1,254 @@
+package courseoutline
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/gocolly/colly"
+	"github.com/shawnyu5/debate_dragon_2.0/commands"
+	"github.com/shawnyu5/debate_dragon_2.0/utils"
+)
+
+// get a course outline
+type Outline struct{}
+
+type CourseInfo struct {
+	// title of course
+	Title string
+	// course description
+	Description string
+	URL         string
+}
+
+// Components implements commands.Command
+func (Outline) Components() []commands.Component {
+	return nil
+}
+
+// Def implements commands.Command
+func (Outline) Def() *discordgo.ApplicationCommand {
+	return &discordgo.ApplicationCommand{
+		Version:     "1.0.0",
+		Type:        0,
+		Name:        "outline",
+		Description: "Find a course outline",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:         discordgo.ApplicationCommandOptionString,
+				Name:         "course_code",
+				Description:  "course code",
+				Required:     true,
+				Autocomplete: true,
+			},
+		},
+	}
+}
+
+// Handler implements commands.Command
+func (Outline) Handler(sess *discordgo.Session, i *discordgo.InteractionCreate) (string, error) {
+	switch i.Type {
+	case discordgo.InteractionApplicationCommandAutocomplete:
+		return GenerateSubjectCodeCompletion(sess, i)
+	default:
+		url := GeneratewebPageURL(sess, i)
+		courseInfo := GetCourseInfo(url)
+		courseInfo.URL = url
+		sess.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Embeds: []*discordgo.MessageEmbed{
+					{
+						URL:         url,
+						Type:        discordgo.EmbedTypeArticle,
+						Title:       fmt.Sprintf("**%s**", courseInfo.Title),
+						Description: courseInfo.Description,
+						Timestamp:   "",
+						Color:       0,
+						// Footer:      &discordgo.MessageEmbedFooter{},
+						// Image:       &discordgo.MessageEmbedImage{},
+						// Thumbnail:   &discordgo.MessageEmbedThumbnail{},
+						// Video:       &discordgo.MessageEmbedVideo{},
+						// Provider:    &discordgo.MessageEmbedProvider{},
+						// Author:      &discordgo.MessageEmbedAuthor{},
+						// Fields:      []*discordgo.MessageEmbedField{},
+					},
+				},
+			},
+		})
+	}
+	return "hello", nil
+}
+
+// GenerateSubjectCodeCompletion get the subject codes to fill in for autocompletion based on current user input
+func GenerateSubjectCodeCompletion(sess *discordgo.Session, i *discordgo.InteractionCreate) (string, error) {
+	input := utils.ParseUserOptions(sess, i)
+	type findSubjectCodesCompletion struct {
+		Label string `json:"label"`
+		Value string `json:"value"`
+	}
+
+	req, err := http.NewRequest(http.MethodGet, "https://www.senecacollege.ca/ssos/jsp/ajaxCalls/findSubjectCodes.jsp?isLoggedIn=", nil)
+	// req, err := http.NewRequest(http.MethodGet, "https://www.senecacollege.ca/ssos/jsp/ajaxCalls/findSubjectCodes.jsp?isLoggedIn=&term=ECN230", nil)
+	if err != nil {
+		return "", err
+	}
+
+	q := req.URL.Query()
+	q.Add("term", input["course_code"].StringValue())
+	req.URL.RawQuery = q.Encode()
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	var subjectCodes []findSubjectCodesCompletion
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	json.Unmarshal(b, &subjectCodes)
+	response := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionApplicationCommandAutocompleteResult,
+		Data: &discordgo.InteractionResponseData{
+			Choices: []*discordgo.ApplicationCommandOptionChoice{},
+		},
+	}
+	for _, code := range subjectCodes {
+		response.Data.Choices = append(response.Data.Choices, &discordgo.ApplicationCommandOptionChoice{
+			Name:  code.Label,
+			Value: code.Value,
+		})
+	}
+	sess.InteractionRespond(i.Interaction, response)
+	return "Filled out subject code for Autocomplete", nil
+
+}
+
+// GeneratewebPageURL constructs a url that leads to the course outline page for the selected course.
+// return: url to the webpage containing course outline
+func GeneratewebPageURL(sess *discordgo.Session, i *discordgo.InteractionCreate) string {
+	input := utils.ParseUserOptions(sess, i)
+	// https://www.senecacollege.ca/ssos/findOutline.do?isLoggedIn=&subjectOrAndTitle=%5BECN230%5D+Making+Sense+of+Our+Economy&schoolCode=
+	req, err := http.NewRequest(http.MethodGet, "https://www.senecacollege.ca/ssos/findOutline.do?", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	q := req.URL.Query()
+	q.Add("subjectOrAndTitle", input["course_code"].StringValue())
+	req.URL.RawQuery = q.Encode()
+	return req.URL.String()
+
+}
+
+// GetCourseInfo scrapes the url passed in to get course information
+// url: the course outline to scrape.
+// return: CourseInfo struct containing the title and description of the course.
+func GetCourseInfo(url string) CourseInfo {
+	c := colly.NewCollector()
+	info := CourseInfo{}
+
+	idx := 0
+	c.OnHTML("div.sectionModification", func(e *colly.HTMLElement) {
+		e.ForEach("div", func(i int, el *colly.HTMLElement) {
+			switch idx {
+			case 1:
+				info.Description = strings.ReplaceAll(el.Text, "Subject Description", "")
+				info.Description = strings.TrimSpace(info.Description)
+				idx++
+			default:
+				idx++
+			}
+		})
+	})
+
+	// the only h1 in the page is the title
+	c.OnHTML("h1", func(e *colly.HTMLElement) {
+		info.Title = strings.TrimSpace(e.Text)
+
+	})
+
+	c.Visit(url)
+	return info
+}
+
+// GetStringInBetween Returns empty string if no start string found
+func GetStringInBetween(str string, start string, end string) (result string) {
+	s := strings.Index(str, start)
+	if s == -1 {
+		return
+	}
+	s += len(start)
+	e := strings.Index(str[s:], end)
+	if e == -1 {
+		return
+	}
+	e += s + e - 1
+	return str[s:e]
+}
+
+// TODO: unable to get school information. Skipping for now
+func CreateSchoolMenu(sess *discordgo.Session, i *discordgo.InteractionCreate) (string, error) {
+	// response from the api for schools that teaches the subject
+	type SchoolCompletion struct {
+		SName string `json:"sName"`
+		Scode string `json:"scode"`
+	}
+	input := utils.ParseUserOptions(sess, i)
+	// https://www.senecacollege.ca/ssos/jsp/ajaxCalls/pullSchoolsThatTeachSubject.jsp
+	req, err := http.NewRequest(http.MethodPost, "https://www.senecacollege.ca/ssos/jsp/ajaxCalls/pullSchoolsThatTeachSubject.jsp", nil)
+	// req, err := http.NewRequest(http.MethodGet, "https://www.senecacollege.ca/ssos/findOutline.do", nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("Content-Type", "application/json, text/javascript, */*; q=0.01")
+
+	q := req.URL.Query()
+	q.Add("subjectTitleCode", input["course_code"].StringValue())
+	// q.Add("subjectOrAndTitle", input["course_code"].StringValue())
+	req.URL.RawQuery = q.Encode()
+	// %5BECN230%5D+Making+Sense+of+Our+Economy
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	var schools []SchoolCompletion
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	// fmt.Printf("CreateSchoolMenu b: %v\n", string(b)) // __AUTO_GENERATED_PRINT_VAR__
+
+	json.Unmarshal(b, &schools)
+	fmt.Printf("CreateSchoolMenu schools: %v\n", schools) // __AUTO_GENERATED_PRINT_VAR__
+	response := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			TTS:             false,
+			Content:         "",
+			Components:      []discordgo.MessageComponent{},
+			Embeds:          []*discordgo.MessageEmbed{},
+			AllowedMentions: &discordgo.MessageAllowedMentions{},
+			Files:           []*discordgo.File{},
+			Flags:           0,
+			Choices:         []*discordgo.ApplicationCommandOptionChoice{},
+			CustomID:        "",
+			Title:           "",
+		},
+	}
+	for _, school := range schools {
+		response.Data.Choices = append(response.Data.Choices, &discordgo.ApplicationCommandOptionChoice{
+			Name:  school.SName,
+			Value: school.Scode,
+		})
+	}
+	// sess.InteractionRespond(i.Interaction, response)
+	return "Filled out school for Autocomplete", nil
+}
--- go.mod
+++ go.mod
@@ -5,21 +5,32 @@ go 1.19
 require (
 	github.com/bwmarrin/discordgo v0.26.2-0.20221023230326-afab84009be8
 	github.com/fogleman/gg v1.3.0
+	github.com/gocolly/colly v1.2.0
 	github.com/spf13/afero v1.9.2
 )
 
 require (
 	github.com/AndreasBriese/bbloom v0.0.0-20190825152654-46b345b51c96 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.5 // indirect
+	github.com/antchfx/xmlquery v1.3.13 // indirect
+	github.com/antchfx/xpath v1.2.1 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/dgraph-io/ristretto v0.0.2 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.1.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
@@ -30,8 +41,6 @@ require (
 	github.com/google/go-querystring v1.1.0
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/olekukonko/tablewriter v0.0.5
-	github.com/onsi/ginkgo/v2 v2.4.0
-	github.com/onsi/gomega v1.23.0
 	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa // indirect
 	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
 	golang.org/x/sys v0.1.0 // indirect
