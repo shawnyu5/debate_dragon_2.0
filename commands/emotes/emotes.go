f6b67f097cd33be2b6107114d45db4e8f60a5a4e
shawn yu <shawn.yu5@hotmail.com>
2022-12-08 22:20:37 -0500

    feat: autocommand preview does not work
    Unable to respond to an interaction while returning auto complete
    result. The idea of having a preview, an empheral message that displays
    the emote while the user types is not possible for now.
    
    Empheral messages can only be sent when the user triggers a slash
    command, due to security reasons...
    
    https://stackoverflow.com/questions/68298805/send-spontaneous-ephemeral-discord-message

--- commands/emotes/emotes.go
+++ commands/emotes/emotes.go
@@ -1,10 +1,7 @@
 package emotes
 
 import (
-	"fmt"
-
 	"github.com/bwmarrin/discordgo"
-	"github.com/lithammer/fuzzysearch/fuzzy"
 	"github.com/shawnyu5/debate_dragon_2.0/commands"
 	"github.com/shawnyu5/debate_dragon_2.0/utils"
 )
@@ -33,18 +30,18 @@ func (Emotes) Def() *discordgo.ApplicationCommand {
 				Name:         "name",
 				Description:  "name of emote",
 				Required:     true,
-				Autocomplete: true,
+				Autocomplete: false,
 			},
 		},
 	}
 
-	// emotes := GetEmotes()
-	// for name, url := range emotes {
-	// def.Options[0].Choices = append(def.Options[0].Choices, &discordgo.ApplicationCommandOptionChoice{
-	// Name:  name,
-	// Value: url,
-	// })
-	// }
+	emotes := GetEmotes()
+	for name, url := range emotes {
+		def.Options[0].Choices = append(def.Options[0].Choices, &discordgo.ApplicationCommandOptionChoice{
+			Name:  name,
+			Value: url,
+		})
+	}
 
 	return def
 }
@@ -52,89 +49,13 @@ func (Emotes) Def() *discordgo.ApplicationCommand {
 // Handler implements commands.Command
 func (Emotes) Handler(sess *discordgo.Session, i *discordgo.InteractionCreate) (string, error) {
 	input := utils.ParseUserOptions(sess, i)
-	sess.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Content: "Preview window",
-			Flags:   discordgo.MessageFlagsEphemeral,
-		},
+	utils.DeferReply(sess, i.Interaction)
+	emoteUrl := input["name"].StringValue()
+	_, err := sess.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+		Content: &emoteUrl,
 	})
-	// err := sess.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-	// Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
-	// Data: &discordgo.InteractionResponseData{
-	// Flags: discordgo.MessageFlagsEphemeral,
-	// },
-	// })
-
-	switch i.Type {
-	case discordgo.InteractionApplicationCommandAutocomplete:
-		fmt.Println("autocomplete Handler") // __AUTO_GENERATED_PRINTF__
-		userInput := input["name"]
-
-		emotes := GetEmotes()
-		choices := []*discordgo.ApplicationCommandOptionChoice{}
-
-		// collect names of all emotes
-		emoteNames := make([]string, len(emotes))
-		idx := 0
-		for name := range emotes {
-			emoteNames[idx] = name
-			idx++
-		}
-
-		if userInput.StringValue() != "" {
-
-			matches := fuzzy.Find(userInput.StringValue(), emoteNames)
-
-			for _, match := range matches {
-				choices = append(choices, &discordgo.ApplicationCommandOptionChoice{
-					Name:  match,
-					Value: emotes[match],
-				})
-
-			}
-		} else {
-			for _, name := range emoteNames {
-				choices = append(choices, &discordgo.ApplicationCommandOptionChoice{
-					Name:  name,
-					Value: emotes[name],
-				})
-			}
-		}
-
-		// if _, found := emotes[userInput.StringValue()]; found {
-		// fmt.Println("Handler") // __AUTO_GENERATED_PRINTF__
-		// content := emotes[userInput.StringValue()]
-		// _, err := sess.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
-		// Content: &content,
-		// })
-		// fmt.Printf("Handler err: %v\n", err) // __AUTO_GENERATED_PRINT_VAR__
-		// }
-
-		sess.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionApplicationCommandAutocompleteResult,
-			Data: &discordgo.InteractionResponseData{
-				Choices: choices,
-			},
-		})
-
-	default:
-		fmt.Println(input["name"].Focused)
-		emoteUrl := input["name"].StringValue()
-		_, err := sess.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
-			Content: &emoteUrl,
-		})
-		// err := sess.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		// Type: discordgo.InteractionResponseChannelMessageWithSource,
-		// Data: &discordgo.InteractionResponseData{
-		// Content: input["name"].StringValue(),
-		// },
-		// })
-
-		if err != nil {
-			return "", err
-		}
-		return "emote sent", nil
+	if err != nil {
+		return "", err
 	}
 	return "emote sent", nil
 }
