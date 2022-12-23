5f31efac3f975d61f3eed4c68378a4dc50d4fc26
shawn yu <shawn.yu5@hotmail.com>
2022-12-22 19:47:15 -0500

    fix: renable all commands

--- main.go
+++ main.go
@@ -8,9 +8,18 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/shawnyu5/debate_dragon_2.0/commands"
+	courseoutline "github.com/shawnyu5/debate_dragon_2.0/commands/courseOutline"
+	"github.com/shawnyu5/debate_dragon_2.0/commands/dd"
+	"github.com/shawnyu5/debate_dragon_2.0/commands/emotes"
+	"github.com/shawnyu5/debate_dragon_2.0/commands/insult"
+	"github.com/shawnyu5/debate_dragon_2.0/commands/ivan"
+	"github.com/shawnyu5/debate_dragon_2.0/commands/manageIvan"
 	"github.com/shawnyu5/debate_dragon_2.0/commands/memes/mock"
 	newmember "github.com/shawnyu5/debate_dragon_2.0/commands/newMember"
+	"github.com/shawnyu5/debate_dragon_2.0/commands/poll"
+	"github.com/shawnyu5/debate_dragon_2.0/commands/rmp"
 	"github.com/shawnyu5/debate_dragon_2.0/commands/snipe"
+	subforcarmen "github.com/shawnyu5/debate_dragon_2.0/commands/subForCarmen"
 	generatedocs "github.com/shawnyu5/debate_dragon_2.0/generate_docs"
 	"github.com/shawnyu5/debate_dragon_2.0/middware"
 	utils "github.com/shawnyu5/debate_dragon_2.0/utils"
@@ -46,17 +55,17 @@ var (
 
 	// array of all slash commands in this bot
 	allCommands = []commands.Command{
-		// manageIvan.ManageIvan{},
-		// poll.Poll{},
-		// dd.DD{},
-		// insult.Insult{},
-		// ivan.Ivan{},
-		// rmp.Rmp{},
-		// subforcarmen.SubForCarmen{},
-		// courseoutline.Outline{},
-		// snipe.Snipe{},
-		// emotes.Emotes{},
-		// newmember.NewMember{},
+		manageIvan.ManageIvan{},
+		poll.Poll{},
+		dd.DD{},
+		insult.Insult{},
+		ivan.Ivan{},
+		rmp.Rmp{},
+		subforcarmen.SubForCarmen{},
+		courseoutline.Outline{},
+		snipe.Snipe{},
+		emotes.Emotes{},
+		newmember.NewMember{},
 		mock.Mock{},
 	}
 
